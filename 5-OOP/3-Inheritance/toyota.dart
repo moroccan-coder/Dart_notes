ad774b531b0da389f49a2518a79e64a768cd7c35
Yassine El Haitar <31771599+moroccan-coder@users.noreply.github.com>
2020-09-26 07:49:50 -0700

    Create toyota.dart

---
+++ 5-OOP/3-Inheritance/toyota.dart
@@ -0,0 +1,19 @@
+import 'car.dart';
+
+class Toyota extends Car{
+
+String _toyotaPerferm;
+
+
+set toyotaPerferm(String peferm) => this._toyotaPerferm = peferm;
+String get toyotaPerferm => this._toyotaPerferm;
+
+
+@override // reuse the super class method
+String showInfo()
+{
+ return super.showInfo() + '\n${this._toyotaPerferm}';
+}
+
+
+}
