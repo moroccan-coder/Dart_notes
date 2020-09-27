d43c8274753d1fb982cc0bd3d6347363bbb1df47
Yassine El Haitar <31771599+moroccan-coder@users.noreply.github.com>
2020-09-27 06:07:50 -0700

    Create fish.dart

---
+++ 5-OOP/4-Abstraction/fish.dart
@@ -0,0 +1,24 @@
+import 'animal.dart';
+
+class Fish extends Animal{
+ 
+@override
+eat()
+{
+ print('Fish is eating herbs');
+}
+
+@override
+move()
+{
+ print('Fish is Swiming into water');
+}
+
+@override
+sleep()
+{
+ print('Fish is Sleeping on the water');
+}
+
+
+}
