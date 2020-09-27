bc5d2e33ea20bf0606506857ffd594051fac979a
Yassine El Haitar <31771599+moroccan-coder@users.noreply.github.com>
2020-09-27 16:30:30 -0700

    Create cat.dart

---
+++ 5-OOP/5-Interface/cat.dart
@@ -0,0 +1,20 @@
+import 'animal.dart';
+
+class Cat implements Animal{
+
+@override
+String name;
+
+@override
+void eat()
+{
+print("the Cat Eat...");
+}
+
+@override
+void sleep()
+{
+print("the Cat Sleep...");
+}
+
+}
