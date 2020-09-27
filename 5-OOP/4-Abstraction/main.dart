106c5e7d24b3c248856d5101aaf19e1374b974c5
Yassine El Haitar <31771599+moroccan-coder@users.noreply.github.com>
2020-09-27 06:06:26 -0700

    Create main.dart

---
+++ 5-OOP/4-Abstraction/main.dart
@@ -0,0 +1,15 @@
+import 'lion.dart';
+import 'fish.dart';
+
+void main()
+
+{
+ //Abstraction
+ var lion1 = new Lion();
+ var fish1 = new Fish();
+
+ lion1.drink();
+ fish1.eat();
+ lion1.eat();
+
+}
