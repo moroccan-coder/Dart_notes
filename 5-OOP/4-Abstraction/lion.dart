03857c5e1a8c33fb9833161cd36cc3652345c825
Yassine El Haitar <31771599+moroccan-coder@users.noreply.github.com>
2020-09-27 06:07:24 -0700

    Create lion.dart

---
+++ 5-OOP/4-Abstraction/lion.dart
@@ -0,0 +1,23 @@
+import 'animal.dart';
+
+class Lion extends Animal{
+
+@override
+eat()
+{
+ print('Lion is eating meat');
+}
+
+@override
+move()
+{
+ print('Lion is Moving by 4 legs');
+}
+
+@override
+sleep()
+{
+ print('Lion is Sleeping...');
+}
+
+}
